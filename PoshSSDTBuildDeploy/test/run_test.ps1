b82661046dd9593982e1f63d78f40b05fe099334
Unknown <richie@bzzzt.io>
2017-11-24 15:19:56 +0000

    removed bat from psm1; add test stuff; ignore bin/debug

---
+++ PoshSSDTBuildDeploy/test/run_test.ps1
@@ -0,0 +1,15 @@
+Clear-Host
+$installVs2017BuildTools = Start-Process -FilePath .\launch_localdb.bat -WorkingDirectory "C:\WINDOWS\System32" -Wait -PassThru  -NoNewWindow
+Import-Module "C:\Users\Richie\Source\Repos\PoshSSDTBuildDeploy\PoshSSDTBuildDeploy" -Force
+$svrConnstring = "SERVER=(LocalDB)\TestDeploy;Integrated Security=True;Database=master"
+$WWI_NAME = "WideWorldImporters"
+$WWI = Join-Path $PSScriptRoot "wwi-dw-ssdt"
+$WWI_SLN = Join-Path $WWI "\WideWorldImportersDW.sqlproj"
+$WWI_DAC = Join-Path $WWI "\Microsoft.Data.Tools.Msbuild\lib\net46"
+$WWI_DACFX = Join-Path $WWI_DAC "\Microsoft.SqlServer.Dac.dll"
+$WWI_DACPAC = Join-Path $WWI "\bin\Debug\WideWorldImportersDW.dacpac"
+$WWI_PUB = Join-Path $WWI "\bin\Debug\WideWorldImportersDW.publish.xml"
+Install-VsBuildTools2017
+Install-MicrosoftDataToolsMSBuild -WorkingFolder $WWI
+Invoke-MsBuildSSDT -DatabaseSolutionFilePath $WWI_SLN -DataToolsFilePath $WWI_DAC 
+Publish-DatabaseDeployment -dacfxPath $WWI_DACFX -dacpac $WWI_DACPAC -publishXml $WWI_PUB -targetConnectionString $svrConnstring -targetDatabaseName $WWI_NAME
