ce437f98fd864f7b38ecddc22d93c298710c1598
Unknown <richie@bzzzt.io>
2017-11-30 17:23:31 +0000

    add msbuild version number
    should be 14.0 or 15.0

--- PoshSSDTBuildDeploy/Functions/InvokeMSBuildSSDT.ps1
+++ PoshSSDTBuildDeploy/Functions/InvokeMSBuildSSDT.ps1
@@ -2,14 +2,23 @@
 
 function Invoke-MsBuildSSDT {
     param ( [string] $DatabaseSolutionFilePath
-        , [string] $DataToolsFilePath)
-        
+        , [string] $DataToolsFilePath
+        , [string]$MSBuildVersionNumber)
+
+    if ([string]::IsNullOrEmpty($MSBuildVersionNumber)) {
+        $MSBuildVersionNumber = "15.0"
+    }
+    if ($MSBuildVersionNumber -ne "15.0") {
         $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
-        if (-not (Test-Path $msbuild)) {
-            Write-Error "No MSBuild installed. Instal Build Tools using 'Install-VsBuildTools2017' and try again!"
-            Throw
-        }
-    $arg1 = "/p:tv=15.0"
+    }
+    else {
+        $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+    }
+    if (-not (Test-Path $msbuild)) {
+        Write-Error "No MSBuild installed. Instal Build Tools using 'Install-VsBuildTools2017', set -MSBuildVersionNumber to 15.0 and try again!"
+        Throw
+    }
+    $arg1 = "/p:tv=$MSBuildVersionNumber"
     $arg2 = "/p:SSDTPath=$DataToolsFilePath"
     $arg3 = "/p:SQLDBExtensionsRefPath=$DataToolsFilePath"
     $arg4 = "/p:Configuration=Debug"
