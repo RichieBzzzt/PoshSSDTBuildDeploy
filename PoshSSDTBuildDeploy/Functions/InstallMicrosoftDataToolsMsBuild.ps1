8dc7081f24e5611884cd63e5cb8b2eaab73c3f60
Richie Lee <richie@bzzzt.io>
2018-04-23 11:22:10 +0100

    add return to folder path of where msbuild data tools is installed

--- PoshSSDTBuildDeploy/Functions/InstallMicrosoftDataToolsMsBuild.ps1
+++ PoshSSDTBuildDeploy/Functions/InstallMicrosoftDataToolsMsBuild.ps1
@@ -20,14 +20,12 @@ function Install-MicrosoftDataToolsMSBuild {
     }
     $TestDotNetVersion = Test-NetInstalled -DotNetVersion "4.6.1"
     Write-Host ".NET Version is $($TestDotNetVersion.DotNetVersion), DWORD Value is $($TestDotNetVersion.DWORD) and Required Version is $($TestDotNetVersion.RequiredVersion)" -ForegroundColor White -BackgroundColor DarkMagenta
-        if ($TestDotNetVersion.DWORD -le 394254)
-        {
-            Throw "Need to install .NET 4.6.1 at least!"
-        }
+    if ($TestDotNetVersion.DWORD -le 394254) {
+        Throw "Need to install .NET 4.6.1 at least!"
+    }
     $nugetInstallMsbuild = "&$NugetExe install Microsoft.Data.Tools.Msbuild -ExcludeVersion -OutputDirectory $WorkingFolder"
     if ($DataToolsMsBuildPackageVersion) {
-        if ($DataToolsMsBuildPackageVersion -lt "10.0.61026")
-        {
+        if ($DataToolsMsBuildPackageVersion -lt "10.0.61026") {
             Write-Error "Lower versions than 10.0.61026 will NOT work with Publish-DatabaseDeployment. For more information, read the post https://blogs.msdn.microsoft.com/ssdt/2016/10/20/sql-server-data-tools-16-5-release/"
             Throw
         }
@@ -35,11 +33,17 @@ function Install-MicrosoftDataToolsMSBuild {
     }
     Write-Host $nugetInstallMsbuild -BackgroundColor White -ForegroundColor DarkGreen
     Invoke-Expression $nugetInstallMsbuild
-    $SSDTMSbuildFolder = "$WorkingFolder\Microsoft.Data.Tools.Msbuild\lib\net46"
-    if (-not (Test-Path $SSDTMSbuildFolder)) {
-        $SSDTMSbuildFolder = "$WorkingFolder\Microsoft.Data.Tools.Msbuild\lib\net40"
-        if (-not (Test-Path $SSDTMSbuildFolder)) {
+    $SSDTMSbuildFolderNet46 = "$WorkingFolder\Microsoft.Data.Tools.Msbuild\lib\net46"
+    if (-not (Test-Path $SSDTMSbuildFolderNet46)) {
+        $SSDTMSbuildFolderNet40 = "$WorkingFolder\Microsoft.Data.Tools.Msbuild\lib\net40"
+        if (-not (Test-Path $SSDTMSbuildFolderNet40)) {
             Throw "It appears that the nuget install hasn't worked, check output above to see whats going on"
         }
+    }
+    if (Test-Path $SSDTMSbuildFolderNet46) {
+        return $SSDTMSbuildFolderNet46
+    }
+    elseif (Test-Path $SSDTMSbuildFolderNet40) {
+        return $SSDTMSbuildFolderNet40
     }
 }
