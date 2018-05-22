a9fd7a3678ea2a4d00b254115ed480c32238bdbf
Eugene Niemand <eugene.niemand@awin.com>
2018-05-22 21:20:33 +0100

    Added the Pester install to Invoke-Tests.ps1

--- tests/Invoke-Tests.ps1
+++ tests/Invoke-Tests.ps1
@@ -1,1 +1,9 @@
+try {
+    Get-InstalledModule Pester -ErrorAction Stop | Out-Null
+}
+catch {
+    write-host "Pester Module not found. Trying to install..."
+    Install-Module Pester -Force -Scope CurrentUser
+}
+
 Invoke-Pester .\*.Tests.ps1
