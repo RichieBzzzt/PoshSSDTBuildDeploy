02059bd6009da299351fcbc92edc5d4b275f1dcb
Eugene Niemand <eugene.niemand@awin.com>
2018-05-23 14:42:22 +0100

    Added MinimumVersion 4.3.1 check to Pester check

--- tests/Invoke-Tests.ps1
+++ tests/Invoke-Tests.ps1
@@ -1,9 +1,9 @@
 try {
-    Get-InstalledModule Pester -ErrorAction Stop | Out-Null
+    Get-InstalledModule Pester -MinimumVersion 4.3.1 -ErrorAction Stop | Out-Null
 }
 catch {
     write-host "Pester Module not found. Trying to install..."
-    Install-Module Pester -Force -Scope CurrentUser
+    Install-Module Pester -MinimumVersion 4.3.1 -Force -SkipPublisherCheck -Scope CurrentUser
 }
 
 Invoke-Pester .\*.Tests.ps1
