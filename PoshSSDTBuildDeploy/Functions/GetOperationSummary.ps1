91d44337323d15dede6859737a92838bb50e797d
Richie Lee <richie@bzzzt.io>
2018-06-15 10:18:33 +0100

    remove write-host

--- PoshSSDTBuildDeploy/Functions/GetOperationSummary.ps1
+++ PoshSSDTBuildDeploy/Functions/GetOperationSummary.ps1
@@ -8,9 +8,6 @@ Function Get-OperationSummary {
         foreach ($item in $operation.Item) {
             $OS += $operation | Select-Object @{Label = "OperationName"; Expression = {($operation.Name)}}, @{Label = "ItemValue"; Expression = {($Item.Value)}}, @{Label = "ItemType"; Expression = {($Item.Type)}}, @{Label = "IssueId"; Expression = {($Item.Issue.Id)}} 
         }
-        foreach ($Issue in $item.Issue) {
-            Write-Host $Issue.Id -ForegroundColor Green
-        }
     }
     Return $OS 
 }
