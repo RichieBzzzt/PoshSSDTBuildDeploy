1db824fd188ccd77a2672a5d83f2b1df494ab48c
Richie Lee <richie@bzzzt.io>
2018-07-31 15:01:36 +0100

    tidy up messages

--- PoshSSDTBuildDeploy/Functions/GetSqlCmdVars.ps1
+++ PoshSSDTBuildDeploy/Functions/GetSqlCmdVars.ps1
@@ -10,30 +10,29 @@ function Get-SqlCmdVars {
     )
 
     # track any missing variables so provide more helpful error messages
-    $missingOctopusVariables = @()
+    $missingPowerShellVariables = @()
     $keys = $($sqlCommandVariableValues.Keys)
     foreach ($var in $keys) {
         # Attempt to resolve SQLCmd variables via matching powershell variables explicitly defined in the current context
         if (Test-Path variable:$var) {
-            Write-Host "Here! again"
             $value = Get-Variable $var -ValueOnly
             Write-Verbose ('Setting SqlCmd variable: {0} = {1}' -f $var, $value)
             $sqlCommandVariableValues[$var] = $value
         }
         else {
-            $missingOctopusVariables += $var
+            $missingPowerShellVariables += $var
         }
     }
 
     # To try and avoid 'bad' default values getting deployed, block a deployment if we have SqlCmd variables that aren't defined in Octopus
     
-    if ($missingOctopusVariables.Count -gt 0) {
+    if ($missingPowerShellVariables.Count -gt 0) {
         if ($PSBoundParameters.ContainsKey('FailOnMissingVariables') -eq $true) {
-            throw ('The following SqlCmd variables are not defined in the Octopus project (but are defined in the publish profile): {0}' -f ($missingOctopusVariables -join " `n"))
+            throw ('The following SqlCmd variables are not defined in the current session (but are defined in the publish profile): {0}' -f ($missingPowerShellVariables -join " `n"))
         }
         else{
-            Write-Host 'The following SqlCmd variables are not defined in the Octopus project (but are defined in the publish profile):'  
-            foreach ($missing in $missingOctopusVariables)
+            Write-Host 'The following SqlCmd variables are not defined in the current session (but are defined in the publish profile):'  
+            foreach ($missing in $missingPowerShellVariables)
             {Write-Host $missing}
         }
     }
