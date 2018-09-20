11d83c5babe46de878cf59daba477f1d8b593a06
Richie Lee <richie@bzzzt.io>
2018-09-20 15:44:46 +0100

    add script to publish to PSGallery

---
+++ PublishToPSGallery.ps1
@@ -0,0 +1,26 @@
+param(
+    [string] $apiKey,
+    [string] $path
+)
+
+$nugetPath = "c:\nuget"
+
+# --- C:\nuget exists on VS2017 build agents. To avoid task failure, check whether the directory exists and only create if it doesn't/
+if (!(Test-Path -Path $nugetPath)) {
+    Write-Verbose "$nugetPath does not exist on this system. Creating directory."
+    New-Item -Path $nugetPath -ItemType Directory
+}
+
+Write-Verbose "Download Nuget.exe to C:\nuget"
+Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile $nugetPath\Nuget.exe
+
+Write-Verbose "Add C:\nuget as %PATH%"
+$pathenv= [System.Environment]::GetEnvironmentVariable("path")
+$pathenv=$pathenv+";"+$nugetPath
+[System.Environment]::SetEnvironmentVariable("path", $pathenv)
+
+Write-Verbose "Create NuGet package provider"
+Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
+
+Write-Verbose "Publishing module"
+Publish-Module -Path $path -NuGetApiKey $apiKey
