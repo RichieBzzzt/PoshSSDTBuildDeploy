11651f47c65730140a1a3bd01e8385720416cd05
Richie Lee <richie@bzzzt.io>
2018-09-21 09:33:56 +0100

    add script to publish to PSGallery

--- PublishToPSGallery.ps1
+++ PublishToPSGallery.ps1
@@ -7,20 +7,20 @@ $nugetPath = "c:\nuget"
 
 # --- C:\nuget exists on VS2017 build agents. To avoid task failure, check whether the directory exists and only create if it doesn't/
 if (!(Test-Path -Path $nugetPath)) {
-    Write-Verbose "$nugetPath does not exist on this system. Creating directory."
+    Write-Verbose "$nugetPath does not exist on this system. Creating directory." -Verbose
     New-Item -Path $nugetPath -ItemType Directory
 }
 
-Write-Verbose "Download Nuget.exe to C:\nuget"
+Write-Verbose "Download Nuget.exe to C:\nuget" -Verbose
 Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile $nugetPath\Nuget.exe
 
-Write-Verbose "Add C:\nuget as %PATH%"
+Write-Verbose "Add C:\nuget as %PATH%" -Verbose
 $pathenv= [System.Environment]::GetEnvironmentVariable("path")
 $pathenv=$pathenv+";"+$nugetPath
 [System.Environment]::SetEnvironmentVariable("path", $pathenv)
 
-Write-Verbose "Create NuGet package provider"
+Write-Verbose "Create NuGet package provider" -Verbose
 Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
 
-Write-Verbose "Publishing module"
+Write-Verbose "Publishing module" -Verbose
 Publish-Module -Path $path -NuGetApiKey $apiKey
