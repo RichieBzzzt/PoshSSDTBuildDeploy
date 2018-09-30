849832ae2614ee9ff4ae5b79034a34a2af542a99
Richard Lee <Richard.Lee@sabin.io>
2018-09-30 20:24:13 +0100

    get tsts working!

--- tests/Install-MicrosoftDataToolsMSBuild.Tests.ps1
+++ tests/Install-MicrosoftDataToolsMSBuild.Tests.ps1
@@ -1,16 +1,16 @@
 #import module from repo
 Import-Module (Join-Path $PSScriptRoot "..\PoshSSDTBuildDeploy") -Force
 Import-Module Pester -Force
-
 Describe "Install-MicrosoftDataToolsMSBuild" {
     $WWI = Join-Path $PSScriptRoot "wwi-dw-ssdt"
+    It "skip install of nuget" {
+        Install-NuGet -WorkingFolder $PSScriptRoot 
+        {Install-MicrosoftDataToolsMSBuild -WorkingFolder $WWI -DataToolsMsBuildPackageVersion "10.0.61026" -NugetPath $PSScriptRoot} | Should -Not -Throw
+    }
     It "should install MicrosoftDataToolsMSBuild 10.0.61026" {
         {Install-MicrosoftDataToolsMSBuild -WorkingFolder $WWI -DataToolsMsBuildPackageVersion "10.0.61026"} | Should -Not -Throw
     }
     it "should throw exception for MicrosoftDataToolsMSBuild 10.0.60809" {
         {Install-MicrosoftDataToolsMSBuild -WorkingFolder $WWI -DataToolsMsBuildPackageVersion "10.0.60809"} | Should -Throw "Lower versions than 10.0.61026 will NOT work with Publish-DatabaseDeployment."       
-    }
-    It "skip install of nuget" {
-        {Install-MicrosoftDataToolsMSBuild -WorkingFolder $WWI -DataToolsMsBuildPackageVersion "10.0.61026" -NugetPath "C:\nuget"} | Should -Not -Throw
     }
 }
