59b1e8d2387b96a56e1cae550154aeb60d42ce4a
B Howard <ben.howard@asos.com>
2019-02-01 13:34:53 +0000

    More tests

--- tests/Install-MicrosoftSqlServerDacFxx64.Tests.ps1
+++ tests/Install-MicrosoftSqlServerDacFxx64.Tests.ps1
@@ -1,32 +1,53 @@
 #import module from repo
 Import-Module (Join-Path $PSScriptRoot "..\PoshSSDTBuildDeploy") -Force
 Import-Module Pester -Force
-Describe "Install-MicrosoftSqlServerDacFxx64" {
-    BeforeAll {
-        $spacePath = Join-Path $PSScriptRoot "s p a c e s"
-        $nuget = Join-Path $spacePath "nuget.exe"
-        Remove-Item $nuget -Force -ErrorAction "SilentlyContinue"
+
+InModuleScope "PoshSSDTBuildDeploy" {
+
+    Describe "Install-MicrosoftSqlServerDacFxx64" {
+        BeforeAll {
+            $spacePath = Join-Path $PSScriptRoot "s p a c e s"
+            $nuget = Join-Path $spacePath "nuget.exe"
+            Remove-Item $nuget -Force -ErrorAction "SilentlyContinue"
+        }
+        $WWI = Join-Path $PSScriptRoot "wwi-dw-ssdt"
+        It "skip install of nuget" {
+            Install-NuGet -WorkingFolder $PSScriptRoot 
+            {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "150.4200.1" -NugetPath $PSScriptRoot} | Should -Not -Throw
+            $expected = Join-Path $wwi "Microsoft.SqlServer.DacFx.x64\lib\net46"
+            $expected | Should -Exist
+        }
+        It "Will throw if nuget does not exist in specified path" {
+            $spaces = Join-Path $PSScriptRoot "s p a c e s"
+            {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DataToolsMsBuildPackageVersion "150.4200.1" -NugetPath $($spaces) } | Should -Throw
+        }
+        It "skip install of nuget; able to use folder path with spaces" {
+            $spaces = Join-Path $PSScriptRoot "s p a c e s"
+            Install-NuGet -WorkingFolder $spaces
+            {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "150.4200.1" -NugetPath $spaces } | Should -Not -Throw
+        }
+        It "should install MicrosoftSqlServerDacFxx64 150.4200.1" {
+            {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "150.4200.1"} | Should -Not -Throw
+        }
+        it "should throw exception for MicrosoftSqlServerDacFxx64 10.0.60809" {
+            {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "130.3450.1"} | Should -Throw "Lower versions than 130.3485.1 will NOT work with Publish-DatabaseDeployment."       
+        }
+
+        context "Old version of dotnet" {
+            # Mock to ensure an old version of dotnet is returned
+            mock Test-NetInstalled {
+                $DotNetInfo = @{ DotNetVersion = 0 ; DWORD = 0; RequiredVersion = $false}
+                return $DotNetInfo
+            }
+        
+            It "Should throw if old version of dotnet installed" {
+                {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "130.3450.1"} | Should -Throw "Need to install .NET 4.6.1 at least!"
+                Assert-MockCalled Test-NetInstalled -Exactly 1
+            }
+        }
+
+
+
     }
-    $WWI = Join-Path $PSScriptRoot "wwi-dw-ssdt"
-    It "skip install of nuget" {
-        Install-NuGet -WorkingFolder $PSScriptRoot 
-        {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "150.4200.1" -NugetPath $PSScriptRoot} | Should -Not -Throw
-        $expected = Join-Path $wwi "Microsoft.SqlServer.DacFx.x64\lib\net46"
-        $expected | Should -Exist
-    }
-    It "Will throw if nuget does not exist in specified path" {
-        $spaces = Join-Path $PSScriptRoot "s p a c e s"
-        {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DataToolsMsBuildPackageVersion "150.4200.1" -NugetPath $($spaces) } | Should -Throw
-    }
-    It "skip install of nuget; able to use folder path with spaces" {
-        $spaces = Join-Path $PSScriptRoot "s p a c e s"
-        Install-NuGet -WorkingFolder $spaces
-        {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "150.4200.1" -NugetPath $spaces } | Should -Not -Throw
-    }
-    It "should install MicrosoftSqlServerDacFxx64 150.4200.1" {
-        {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "150.4200.1"} | Should -Not -Throw
-    }
-    it "should throw exception for MicrosoftSqlServerDacFxx64 10.0.60809" {
-        {Install-MicrosoftSqlServerDacFxx64 -WorkingFolder $WWI -DacFxx64Version "130.3450.1"} | Should -Throw "Lower versions than 130.3485.1 will NOT work with Publish-DatabaseDeployment."       
-    }
+
 }
